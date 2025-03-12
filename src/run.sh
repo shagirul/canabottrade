a97d365a308553c1778efc8cabd0bb4d9075f082
shagirul <shagirul@gmail.com>
2025-03-12 15:21:00 +0530

    initial commit

---
+++ src/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source ../.venv/bin/activate
+
+# Run the Python script
+python main.py 
+
+# Deactivate the virtual environment (optional)
+deactivate
